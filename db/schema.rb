3b151d43c77e4c17b9b7f74db83c989bef4a6ec0
Oleksandr Papevis <sasha@papevis.com>
2021-06-11 06:19:33 +0300

    merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_15_111934) do
-
-  create_table "payloads", charset: "utf8", force: :cascade do |t|
-    t.integer "denomination"
-    t.integer "quantity"
+ActiveRecord::Schema.define(version: 20_190_115_111_934) do
+  create_table 'payloads', charset: 'utf8', force: :cascade do |t|
+    t.integer 'denomination'
+    t.integer 'quantity'
   end
-
 end
