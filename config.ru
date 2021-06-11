0869381fd1acef29a4b7450e34d64dd5a27d8861
Oleksandr Papevis <sasha@papevis.com>
2021-06-11 06:03:34 +0300

    Revert "Initial commit"
    This reverts commit 7d4cf12110f573f5d5e8dbb3cbdc075127fdb1ec.

--- config.ru
+++
@@ -1,3 +0,0 @@
-require File.expand_path('../app/api/v1/api', __FILE__)
-
-run Application
--- spec/features/is_alive_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe "Api is running", type: :feature do
-  it "responds with status ok" do
-    get 'api/v1/status'
-
-    expect(json_response[:status]).to eq 'ok'
-  end
-end
